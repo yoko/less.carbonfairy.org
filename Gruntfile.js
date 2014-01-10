85cb3d360c410da50a458401f7e9513ac52b08e8
yoko <yoko@carbonfairy.org>
2014-01-11 05:27:53 +0900

    ファイル整理

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: 'images/',
-					src: ['*.{png,jpg,gif}'],
+					src: ['**/*.{png,jpg,gif}'],
 					dest: 'images/',
 				}],
 			},
